4454c90bb32cce3ec6adaa70d425d25779428cc4
DESKTOP-DMGLT16\Ela <e.kaczmarek01@gmail.com>
2018-04-04 20:17:46 +0200

    objects from mockus

--- codeWithDesigner.py
+++ codeWithDesigner.py
@@ -13,8 +13,8 @@ FONT = pygame.font.Font(None, 32)
 class Label:
     font = pygame.font.Font(None, 50)
 
-    def __init__(self,x,y, text):
-        self.t1 = self.font.render(text, True, (0, 0, 0))
+    def __init__(self,x,y, obraz):
+        self.t1 = pygame.image.load(obraz)
         self.t_rect1 = self.t1.get_rect()
         self.t_rect1.centerx, self.t_rect1.centery = x,y
 
@@ -78,19 +78,27 @@ class InputBox:
         else: return 0
 
 def main():
+    #zegar
     clock = pygame.time.Clock()
+
+    #todo clock
+    #https://stackoverflow.com/questions/30720665/countdown-timer-in-pygame
+
     input_box1 = InputBox(500, 200, 140, 32)
     input_box2 = InputBox(500, 300, 140, 32)
     input_boxes = [input_box1, input_box2]
     done = False
 
-    label1 = Label(400, 215, "Nickname")
+    label0 = Label(500, 125, "sign_in.png")
+    t0, t_rect0 = label0.getData()
+
+    label1 = Label(400, 215, "nickname.png")
     t1, t_rect1 = label1.getData()
 
-    label2 = Label(400, 315, "Password")
+    label2 = Label(400, 315, "password.png")
     t2, t_rect2 = label2.getData()
 
-    x_button_exit, y_button_exit = 600, 400
+    x_button_ok, y_button_ok = 600, 400
     varial = False
 
     while not done:
@@ -98,11 +106,13 @@ def main():
         screen.blit(t2, t_rect2)
 
         for event in pygame.event.get():
+            #zakonczenie gry
             if event.type == pygame.QUIT:
                 done = True
+            #wciśnięcie przycisku
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 mouse = pygame.mouse.get_pos()
-                if y_button_exit<= mouse[1] <= y_button_exit+50 and x_button_exit-100<=mouse[0]<=x_button_exit:
+                if y_button_ok<= mouse[1] <= y_button_ok+50 and x_button_ok-100<=mouse[0]<=x_button_ok:
                     # dodanie akcji do przycisku - logowanie z bd, przejscie do ekranu kolejnego :)
                     varial = True
                     answer = input_box1.checkWithMongo(input_box1.text, input_box2.text)
@@ -119,10 +129,11 @@ def main():
         for box in input_boxes:
             box.draw(screen)
 
+        screen.blit(t0, t_rect0)
         screen.blit(t1, t_rect1)
         screen.blit(t2, t_rect2)
 
-        login = Buttonify('exit.png', (x_button_exit, y_button_exit), screen)
+        login = Buttonify('ok.png', (x_button_ok, y_button_ok), screen)
 
         pygame.display.flip()
 
