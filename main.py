73164d00e7f8dd16bd428a47388cd6d4b901b104
DESKTOP-DMGLT16\Ela <e.kaczmarek01@gmail.com>
2018-05-10 11:00:28 +0200

    for Mariusz

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import Classes.Login_screen as ls
 if __name__ == "__main__":
     login = ls.Login_screen()
     login.loop()
-    
+
     # for Mariusz:
     # u góry do zakoemntowania bo baza u Cb nie zadziala, na dole do odkomentowania :)
     #  p.Player(500, 50)
